6f6a6ab55a55d5e51078965e81071c736ff418cb
skovic <skovic@lenovo.com>
2017-01-11 11:23:53 -0500

    Add support for turning on/off LEDs - working ver

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -19,19 +19,37 @@ module Api
       end
     end
 
-    def turn_on_loc_led_resource(server)
-      task_id = queue_object_action(server, desc, :method_name => "turn_on_loc_led", :role => "ems_operations")
-      action_result(true, desc, :task_id => task_id)
-      rescue => err
-        action_result(false, err.to_s)
+   def turn_on_loc_led_resource(type, id, _data) 
+      raise BadRequestError, "Must specify an id for turning on a #{type} resource" unless id  
+
+      api_action(type, id) do |klass|
+        server = resource_search(id, type, klass) 
+        api_log_info("Turning on #{klass} #{server}")
+        api_log_info("#{server_ident(server)}")
+        desc = "Turn on Loc LED"
+        task_id = queue_object_action(server, desc, :method_name => "turn_on_loc_led", :role => "ems_operations") 
+        action_result(true, desc, :task_id => task_id) 
+      end 
     end
 
-    def turn_off_loc_led_resource(server)
-      task_id = queue_object_action(server, desc, :method_name => "turn_off_loc_led", :role => "ems_operations")
-      action_result(true, desc, :task_id => task_id)
-      rescue => err
-        action_result(false, err.to_s)
+    def turn_off_loc_led_resource(type, id, _data) 
+      raise BadRequestError, "Must specify an id for turning off a #{type} resource" unless id  
+
+      api_action(type, id) do |klass|
+        server = resource_search(id, type, klass) 
+        api_log_info("Turning off #{klass} #{server}")
+        api_log_info("#{server_ident(server)}") 
+        desc = "Turn off Loc LED"
+        task_id = queue_object_action(server, desc, :method_name => "turn_off_loc_led", :role => "ems_operations") 
+        action_result(true, desc, :task_id => task_id) 
+      end 
     end
+
+    private 
+
+    def server_ident(server)
+      "Server id:#{server.id} name:'#{server.name}'"
+    end
+
   end
 end
-

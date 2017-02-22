efba1a57da89082558376977af7ca9485028ae41
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2017-02-22 14:04:08 -0300

    [PhysicalServerIcons] Adds methods and attributes 1. Some modifies, such as: - Adding VendorTypes to physical_server - Adding MiqPolicyMixin - Implementation of hook method has_compliance_policies? - Implementation of method health_status - Implementation of method label_for_vendor
    All this changes is in physical_server model

--- app/models/physical_server.rb
+++ app/models/physical_server.rb
@@ -1,5 +1,6 @@
 class PhysicalServer < ApplicationRecord
   include NewWithTypeStiMixin
+  include MiqPolicyMixin
   include_concern 'Operations'
 
   acts_as_miq_taggable
@@ -7,12 +8,32 @@ class PhysicalServer < ApplicationRecord
   has_many :firmwares, :foreign_key => "ph_server_uuid", :primary_key => "uuid"
   has_one :host, :foreign_key => "service_tag", :primary_key => "serialNumber"
 
+  VENDOR_TYPES = {
+    # DB            Displayed
+    "lenovo"          => "lenovo",
+    "unknown"         => "Unknown",
+    nil               => "Unknown",
+  }
+
   def name_with_details
     details % {
       :name => name,
     }
   end
 
+  def has_compliance_policies?
+    _, plist = MiqPolicy.get_policies_for_target(self, "compliance", "physicalserver_compliance_check")
+    !plist.blank?
+  end
+
+  def health_status
+    self.healthState
+  end
+
+  def label_for_vendor
+   # "lenovo"
+    VENDOR_TYPES[self.vendor]
+  end
 
   def is_refreshable?
     refreshable_status[:show]
